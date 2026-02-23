293e3dd3940798a64a00f0c88f8de92d7aea684f
Adirexx <ad1234gupta@gmail.com>
2026-02-23 07:16:16 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 // Dynamic version - will be replaced at build time
 // For localhost, use timestamp; for production, use build timestamp
-// Updated at: FEB23_MAJOR_UPDATE_V2.6
+// Updated at: FEB23_MAJOR_UPDATE_V2.7
 const VERSION = self.registration.scope.includes('localhost')
   ? Date.now().toString()
-  : 'BUILD_20260223_MAJOR_V2.6';
+  : 'BUILD_20260223_MAJOR_V2.7';
 
 const CACHE_NAME = `plusopinion-pwa-${VERSION}`;
 
