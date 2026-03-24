1c4258e86b8377eb96898af32426bd5cfd45cb5b
Adirexx <ad1234gupta@gmail.com>
2026-03-25 01:37:15 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Updated at: MAR24_2026_V10.2.0
 const VERSION = self.registration.scope.includes('localhost')
   ? Date.now().toString()
-  : 'BUILD_20260324_V10.2.0';
+  : 'BUILD_20260324_V10.3.0';
 
 const CACHE_NAME = `plusopinion-pwa-${VERSION}`;
 const SUPABASE_HOSTNAME = 'ogqyemyrxogpnwitumsr.supabase.co';
