450078d7c56d86c51772c6716f8b9fcb4f084f63
Adirexx <ad1234gupta@gmail.com>
2026-09-14 01:30:00 +0530

    Fix comment formatting and add version definition

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 // Dynamic version - will be replaced at build time
-// Updated at: AUG26_2026_V9.4.0const VERSION = self.registration.scope.includes('localhost')
+// Updated at: AUG26_2026_V9.4.0
+const VERSION = self.registration.scope.includes('localhost')
   ? Date.now().toString()
   : 'BUILD_20260826_V9.4.0';
 
